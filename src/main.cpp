b8f708934473e07b811b434b2800eb2599e2d644
Alec Roques <alec.roques@gmail.com>
2018-09-01 15:27:27 -0500

    Refactor main to get strings from stdin then use those strings to build the BST

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,34 @@
 #include "split_str.hpp"
 
 // Prototypes
+std::vector<std::string> get_strings_from_stdin(int argc, char* argv[]);
 std::ifstream open_file(char* argv[]);
-BST build_bst();
+BST build_bst(std::vector<std::string> strings);
 
 int main(int argc, char* argv[])
 {
-    std::ifstream file;
-
     if (argc > 2)
     {
         std::cout << "Too many arguments." << std::endl;
         std::exit(EXIT_FAILURE);
     }
+    
+    std::vector<std::string> strings = get_strings_from_stdin(argc, argv);
+    
+    BST bst = build_bst(strings);
+
+    bst.print_preorder();
+    bst.print_inorder();
+    bst.print_postorder();
+
+    return 0;
+}
+
+std::vector<std::string> get_strings_from_stdin(int argc, char* argv[])
+{
+    std::vector<std::string> strings;
+    std::ifstream file;
+
     if (argc == 2) // Filename provided in argv[1]
     {
         file = open_file(argv);
@@ -25,15 +41,23 @@ int main(int argc, char* argv[])
         std::cin.rdbuf(file.rdbuf());
     }
 
-    BST bst = build_bst();
+    std::string line;
 
-    bst.print_preorder();
-    bst.print_inorder();
-    bst.print_postorder();
+    std::vector<std::string> line_strings;
+
+    while (std::getline(std::cin, line))
+    {
+        line_strings = split_str(line, ' ');
+
+        for (auto str : line_strings) 
+        { 
+            strings.push_back(str); 
+        }
+    }
 
     if (file.is_open()) { file.close(); };
 
-    return 0;
+    return strings;
 }
 
 std::ifstream open_file(char* argv[])
@@ -51,19 +75,11 @@ std::ifstream open_file(char* argv[])
     return file;
 }
 
-BST build_bst()
+BST build_bst(std::vector<std::string> strings)
 {
     BST bst;
 
-    std::string line;
-    std::vector<std::string> words;
-
-    while (std::getline(std::cin, line))
-    {
-        words = split_str(line, ' ');
-
-        for (auto word : words) { bst.insert(word); };
-    }
+    for (auto str : strings) { bst.insert(str); };
 
     return bst;
 }
